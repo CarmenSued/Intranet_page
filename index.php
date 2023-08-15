5bf05937c612e9cd6b0b3d750fd6e4620f5d24ff
CarmenSued <61785058+CarmenSued@users.noreply.github.com>
2023-08-15 17:38:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--Carmen J.Sedeno -->
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>Web Programming using PHP - FMA</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+
+<body>
+
+    <ul>
+        <li><a href="intranet.php">Intranet</a></li>
+        <li><a href="administrator.php">Administrator</a></li>
+        <li>Home</li>
+        </ul>
+    <h1>Department of Computer Science and Information Systems</h1>
+
+    <p>The Department of Computer Science and Information Systems at Birkbeck is one of the first computing 
+        departments established in the UK, celebrating our 60th anniversary in 2017. We provide a stimulating 
+        teaching and research environment for both part-time and full-time students, and a friendly, inclusive space for learning, 
+        working and collaborating.
+    </p>
+
+
+
+</body>
+</html>
+
+
